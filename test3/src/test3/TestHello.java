1b64fbda004c0a6f2ea97b75b37e722011601435
kensin9470 <kensin9470@naver.com>
2015-11-11 11:34:22 +0900

    222222

--- test3/src/test3/TestHello.java
+++ test3/src/test3/TestHello.java
@@ -5,6 +5,7 @@ public class TestHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("방갑습니다.");
+		System.out.println("222222222");
 	}
 
 }
